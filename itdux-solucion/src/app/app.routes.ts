20d4cb18f86bb774fbb612e8ece66415309ec724
Nico_Arce_J <nicolas.arce.jalacuri@gmail.com>
2026-05-16 17:27:30 -0400

    Frontend

--- itdux-solucion/src/app/app.routes.ts
+++ itdux-solucion/src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './pages/login/login';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { ImportChat } from './pages/import-chat/import-chat';
+import { AiResult } from './pages/ai-result/ai-result';
+import { FollowUps } from './pages/follow-ups/follow-ups';
+import { Insights } from './pages/insights/insights';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+  },
+  {
+    path: 'importar',
+    component: ImportChat,
+  },
+  {
+    path: 'resultado-ia',
+    component: AiResult,
+  },
+  {
+    path: 'seguimientos',
+    component: FollowUps,
+  },
+  {
+    path: 'insights',
+    component: Insights,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
